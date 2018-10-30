3df73afe259e21cb52b6e5e6f7a84e0ec960c7b0
dineshnanda13 <dannynanda76@gmail.com>
2018-10-29 23:15:55 -0400

    Sign Up System Added

---
+++ Sign up~/HotelMenuSystem/src/FromSignUptoLogin.java
@@ -0,0 +1,22 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SONY INDIA
+ */
+public class FromSignUptoLogin {
+     public static void main(String[] args) {
+        SignUp nf =new SignUp();
+        nf.setVisible(true); 
+     
+     
+     }
+    
+}
